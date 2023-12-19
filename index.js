b151a125080be1bf15e7d8452bb07a94f56064db
Santhosh J <santhosh.jayaraman@sentinelfox.com>
2023-12-19 12:25:46 +0530

    pm2

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 const app = express();
+const os = require('os');
+const path = require('path');
 
-app.use(express.static('public'));
+const hostname = os.hostname();
+
+// app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-	res.sendFile(__dirname + '/public/index.html');
+	res.render('index', {hostname});
 });
 
 const PORT = process.env.PORT || 3000;
